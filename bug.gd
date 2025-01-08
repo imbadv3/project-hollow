b265e0394ae39c29cd5a446c0f13e585ff590705
imbadv3 <sugarhappyv3@gmail.com>
2025-01-08 16:33:22 -0500

    Add files via upload

---
+++ bug.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+var gravity = 2500
+
+var timer = 60
+
+var health = 3
+
+
+func _physics_process(delta: float) -> void:
+	print(health)
+	timer += -1
+	if timer == -60:
+		timer = 60
+	
+	velocity.x = 300
+	if velocity.x == 300 and timer < 0 :
+		velocity.x = -300
+	
+	
+	
+	velocity.y += gravity * delta
+	#print(position)
+	
+	#print(timer)
+	
+	
+	
+	
+	
+	
+	 
+	
+	
+	
+	
+	
+	
+	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if Input.is_action_pressed("spacebar"):
+		health += -1
---
+++ character_body_2d.gd
@@ -0,0 +1,65 @@
+extends CharacterBody2D
+
+var run_speed = 400
+var jump_speed = -1500
+var gravity = 2500
+var health = 20
+const max_health = 10
+var heal_amount = 1
+var stickdg = 1
+
+
+func get_input():
+	velocity.x = 0
+	var right = Input.is_action_pressed('ui_right')
+	var left = Input.is_action_pressed('ui_left')
+	var jump = Input.is_action_just_pressed('ui_up')
+	var heal = Input.is_action_just_pressed('ui_down')
+	var atk = Input.is_action_pressed("spacebar")
+	
+	
+	
+	
+	if is_on_floor() and jump:
+		velocity.y = jump_speed
+	
+	if right:
+		velocity.x += run_speed
+	
+	if left:
+		velocity.x -= run_speed
+	
+	if health < 0:
+		position = Vector2(0,0)
+		
+	if heal:
+		health += heal_amount
+	
+	if health > max_health:
+		health = max_health
+	
+	
+	
+	
+	
+	
+	
+	
+	
+func _physics_process(delta):
+	
+	print(health)
+	velocity.y += gravity * delta
+	
+	get_input()
+	
+	move_and_slide()
+	
+
+
+
+
+
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	health += -1
